4226cfa58a7e33b25ce4fc27b4e76a5db381a4f8
SaiprasadKompally <kompallysaiprasad3@gmail.com>
2026-06-18 15:33:42 +0530

    Update models.py

--- models.py
+++ models.py
@@ -75,7 +75,11 @@ class ModelWrapper:
             #     model_name,
             #     torch_dtype=(torch.bfloat16 if torch.cuda.is_available() else torch.float32),
             # )
-            self.model = AutoModelForCausalLM.from_pretrained(model_name,torch_dtype=torch.bfloat16,device_map="auto")
+            self.model = AutoModelForCausalLM.from_pretrained(
+                model_name,
+                torch_dtype=torch.bfloat16,
+                device_map="auto"
+            )
         if len(self.tokenizer) != self.model.get_input_embeddings().weight.shape[0]:
             self.model.resize_token_embeddings(len(self.tokenizer))
         self.model.to(device)
